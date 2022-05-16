a92ab003787f15c1e50faeaef3c6b1b3df261687
GBFredrik <fredrik.gerdin.borjesson@hotmail.com>
2022-05-16 12:44:34 +0200

    Implement SviVolSurface methods

--- ql/experimental/volatility/svivolsurface.cpp
+++ ql/experimental/volatility/svivolsurface.cpp
@@ -18,11 +18,72 @@
 */
 
 #include <ql/experimental/volatility/svivolsurface.hpp>
+#include <algorithm>
 
 namespace QuantLib {
+    SviVolSurface::SviVolSurface(const Date& referenceDate,
+                                 const Handle<Quote>& spot,
+                                 const Handle<YieldTermStructure>& riskFreeTS,
+                                 const Handle<YieldTermStructure>& dividendTS,
+                                 const std::vector<Date>& expiries,
+                                 const std::vector<std::vector<double> >& smileSectionParameterSets,
+                                 const Calendar& cal,
+                                 BusinessDayConvention bdc,
+                                 const DayCounter& dc)
+    : BlackVarianceTermStructure(referenceDate, cal, bdc, dc), spot_(spot), rf_(riskFreeTS),
+      div_(dividendTS), expiries_(expiries), smileSectionParameterSets_(smileSectionParameterSets) {
+        QL_REQUIRE(expiries_.size() > 0, "VolatilitySviSurfaceConfig expects one or more expiries");
+        QL_REQUIRE(
+            smileSectionParameterSets_.size() > 0,
+            "VolatilitySviSurfaceConfig expects one or more SVI smile section parameter sets");
+        QL_REQUIRE(expiries_.size() == smileSectionParameterSets_.size(),
+                   "Expected an equal amount of expiries andSVI smile section parameter sets");
+        QL_REQUIRE(std::is_sorted(expiries_.begin(), expiries_.end()),
+                   "The SviSmileSections must be sorted by expiry, in increasing order");
 
-    Real SviVolSurface::blackVarianceImpl(Time t, Real strike) const {
-        return Real();
+        for (Size i = 0; i < expiries_.size(); ++i) {
+            smileSections_.push_back(ext::make_shared<SviSmileSection>(
+                expiries[i], forward(expiries[i]), smileSectionParameterSets_[i], dayCounter()));
+            expiryTimes_.push_back(smileSections_[i]->exerciseTime());
+        }
     }
 
+    Real SviVolSurface::blackVarianceImpl(Time t, Real strike) const {
+        // Hard-coded variance term interpolation
+
+        Time t1;
+        Time t2;
+        Real var_t1;
+        Real var_t2;
+        Real moneyness = strike / spot_->value() * div_->discount(t) / rf_->discount(t);
+
+        if (t <= expiryTimes_.front()) {
+            t1 = 0.0;
+            t2 = expiryTimes_.front();
+            var_t1 = 0.0;
+            var_t2 = smileSections_[0]->variance(moneyness * smileSections_[0]->atmLevel());
+        } else if (0) {
+            Size i1, i2;
+            for (Size i = 0; i < smileSections_.size(); ++i) {
+                if (t > smileSections_[i]->exerciseTime()) {
+                    i1 = i;
+                    i2 = i + 1;
+                    break;
+                }
+            }
+            t1 = smileSections_[i1]->exerciseTime();
+            t2 = smileSections_[i2]->exerciseTime();
+            var_t1 = smileSections_[i1]->variance(moneyness * smileSections_[i1]->atmLevel());
+            var_t2 = smileSections_[i2]->variance(moneyness * smileSections_[i2]->atmLevel());
+        } else {
+            return smileSections_.back()->variance(moneyness * smileSections_.back()->atmLevel()) *
+                   t / smileSections_.back()->exerciseTime();
+        }
+        Real var_t = var_t1 + (var_t2 - var_t1) * (t - t1) / (t2 - t1);
+        return var_t;
+    }
+
+    Real SviVolSurface::forward(const Date& expiry) {
+        return spot_->value() * div_->discount(expiry) / rf_->discount(expiry);
+    }
 }
