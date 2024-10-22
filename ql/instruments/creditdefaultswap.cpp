a31219e5a86f651027aeeadfd893231f655dce9f
Matthias Groncki <matthias.groncki@acadia.inc>
2024-10-22 19:33:35 +0200

    QPR-12839 includeSettlementFlow in current accrual rebate
    current accrualrebate is needed to compute the clean fair spread. Fix a bug that appears on IMM-1d, that the coupon npv is of the current period is taken into the coupon leg npv but the accrualrebate is 0.

--- ql/instruments/creditdefaultswap.cpp
+++ ql/instruments/creditdefaultswap.cpp
@@ -262,7 +262,7 @@ namespace QuantLib {
                 effectiveUpfrontDate_);
             Date current = std::max((Date)Settings::instance().evaluationDate(), tradeDate_);
             accrualRebateCurrent_ = boost::make_shared<SimpleCashFlow>(
-                CashFlows::accruedAmount(leg_, false, current + 1),
+                CashFlows::accruedAmount(leg_, true, current + 1),
                 schedule_.calendar().advance(current, cashSettlementDays_, Days,
                                              paymentConvention_));
         }
