c1b23d6cd035232aa1a57a73520258ebd49b13cd
Sverrir <sverrir.thorvaldsson@seb.se>
2025-05-16 00:50:15 +0200

    Add support fro segment nodes, zero nodes, and side
    Add support fro segment nodes, zero nodes (which are sort of hacks) and side

--- ql/math/interpolations/bsplineinterpolation/bsplinestructure.cpp
+++ ql/math/interpolations/bsplineinterpolation/bsplinestructure.cpp
@@ -21,6 +21,7 @@
 // ReSharper disable CppClangTidyBugproneNarrowingConversions
 #include "bsplinestructure.hpp"
 #include "splineconstraints.hpp"
+#include "ql/math/randomnumbers/latticerules.hpp"
 #include <ql/errors.hpp>
 #include <ql/shared_ptr.hpp>
 #include <ql/types.hpp>
@@ -44,41 +45,171 @@ namespace QuantLib {
     // constraints.
     BSplineStructure::BSplineStructure(
         const std::vector<ext::shared_ptr<BSplineSegment>>& splineSegments,
-        const ext::shared_ptr<SplineConstraints>& splineConstraints) :
-        splineSegments_(splineSegments),
-        splineConstraints_(splineConstraints) {
-        std::cout << "Structure with " << splineSegments.size() << " segments" << std::endl;
+        const ext::shared_ptr<SplineConstraints>& splineConstraints,
+        bool useSegmentNodes, bool rejectZeroNode) :
+        splineSegments_(splineSegments), splineConstraints_(splineConstraints),
+    useSegmentNodes_(useSegmentNodes), rejectZeroNode_(rejectZeroNode) {
+        // Create a vector to store the second element of the range for all but the last segment
+        segmentNodes_.reserve(splineSegments.size() - 1); // Reserve space for efficiency
+
+        for (Size i = 0; i < splineSegments.size() - 1; ++i) {
+            auto range = splineSegments[i]->range(); // Assume range() returns std::pair
+            segmentNodes_.push_back(range.second);   // Add the second element to the vector
+        }
     }
 
-    void BSplineStructure::addInterpolationNodes(const std::vector<Real>& interpolationNodes) const {
+    void BSplineStructure::addInterpolationNodes(const std::vector<Real>& interpolationNodes,
+                                            BSplineSegment::Side side, Size nParameters) const {
         const Size nInterpolationNodes = interpolationNodes.size();
         const Size nConstraints = splineConstraints_->getNConstraints();
         const Size nVariables = splineConstraints_->getNumVariables();
 
         for (const Real interpolationNode : interpolationNodes) {
-            Eigen::VectorXd row = evaluateAll(interpolationNode);
+            Eigen::VectorXd row = evaluateAll(interpolationNode, side);
             splineConstraints_->addLinearConstraint(row, 0.0, SplineConstraints::ConstraintType::Equal);
         }
 
         // We still have the old constraint count
-        Eigen::SparseMatrix<Real> B_new(nConstraints + nInterpolationNodes, nInterpolationNodes);
-        for (Size i = 0; i < nInterpolationNodes; ++i) {
-            B_new.insert(nConstraints + i, i) = 1.0;
+        Eigen::SparseMatrix<Real> B_new(nConstraints + nInterpolationNodes,
+                                        nParameters+nInterpolationNodes);
+        for (Size i = 0; i < nInterpolationNodes ; ++i) {
+            B_new.insert(nConstraints + i, nParameters+i) = 1.0;
         }
 
-        Eigen::SparseMatrix<double> C_new(nVariables, nInterpolationNodes);
+        Eigen::SparseMatrix<double> C_new(nVariables, nParameters+nInterpolationNodes);
 
         splineConstraints_->addParameters(nInterpolationNodes, B_new, C_new);
     }
 
+    // Here we interpolate by adding linear constraints to the effect of interpolating
+    // If the system would become overdetermined (there already may be conditions for continuity and such,
+    // as for Hermite interpolation), then one can instead solve a quadratic problem finding the point 
+    // minimizing the norm squared of A x - b, rather than solving it. We can indeed take any part of this
+    // system and move it to the objective function for the same purpose. This is hacked in here post-hoc, but
+    // awaits a better setup.
     Eigen::VectorXd
-    BSplineStructure::interpolate(const std::vector<Real>& interpolationNodes,
-                                  const std::vector<Real>& values) const {
+    BSplineStructure::interpolate(const std::vector<Real>& interpolationNodesOrg,
+                                  const std::vector<Real>& valuesOrg) {
+        std::vector<Real> interpolationNodes, values;
+        // TODO this is a hack, bootstrapper adds 0.0 as a node, which may conflict with our setup,
+        // e.g. if we have an extrapolation to the left 
+        if (interpolationNodesOrg[0] == 0.0 && rejectZeroNode_) {
+            interpolationNodes.reserve(interpolationNodesOrg.size() - 1);
+            values.reserve(interpolationNodesOrg.size() - 1);
+            interpolationNodes.insert(interpolationNodes.end(), interpolationNodesOrg.begin() + 1,
+                                      interpolationNodesOrg.end());
+            values.insert(values.end(), valuesOrg.begin() + 1, valuesOrg.end());
+        } else {
+            interpolationNodes.reserve(interpolationNodesOrg.size());
+            values.reserve(interpolationNodesOrg.size());
+            interpolationNodes.insert(interpolationNodes.end(), interpolationNodesOrg.begin(),
+                                      interpolationNodesOrg.end());
+            values.insert(values.end(), valuesOrg.begin(), valuesOrg.end());
+        }
+        std::vector<Real> transformedValues = transform(interpolationNodes, values);
+        const Size nConstraintsBefore = splineConstraints_->getNConstraints();
+
         splineConstraints_->push();
         addInterpolationNodes(interpolationNodes);
-        Eigen::VectorXd solution = solve(transform(interpolationNodes, values));
-        splineConstraints_->pop();
+
+        // The segment nodes is another semi-hack, the idea to assign special nodes to have a special role actually
+        // is a reasonable way to push non-linearity to the bootstrapper and keep the curve problem in each
+        // iteration convex. But it is probably better to assign these nodes explicitly in the class. Here
+        // we use this to allow for different interpolation spaces in different segments, and tie them
+        // by requiring transformed values from left and right to be the same. This was done to incorporate RT
+        // interpolation.
+        if (useSegmentNodes_) {
+            std::vector<Real> segmentNodes, segmentNodeValues;
+            //extendedNodes.reserve(interpolationNodes.size());
+            //extendedNodes.insert(extendedNodes.end(), interpolationNodes.begin(),
+            //                     interpolationNodes.end());
+            segmentNodes.reserve(segmentNodes_.size());
+            segmentNodeValues.reserve(segmentNodes_.size());
+
+            auto lower = interpolationNodes.begin();
+
+            for (Real segmentNode : segmentNodes_) {
+                lower = std::lower_bound(lower, interpolationNodes.end(),segmentNode - tolerance_);
+                if (lower != interpolationNodes.end()  && * lower <= segmentNode + tolerance_) {
+                    const Size index = std::distance(interpolationNodes.begin(), lower);
+                    segmentNodes.emplace_back(segmentNode);
+                    segmentNodeValues.emplace_back(values[index]);
+                }
+            }
+
+            //extendedNodes.reserve(extendedNodes.size() + segmentNodes.size());
+            //extendedNodes.insert(extendedNodes.end(), segmentNodes.begin(), segmentNodes.end());
+
+            std::vector<Real> transformedValues2 =
+                transform(segmentNodes, segmentNodeValues, BSplineSegment::Side::Left);
+
+            transformedValues.reserve(transformedValues.size() + transformedValues2.size());
+            transformedValues.insert(transformedValues.end(), transformedValues2.begin(),
+                                      transformedValues2.end());
+            addInterpolationNodes(segmentNodes, BSplineSegment::Side::Left, interpolationNodes.size());
+        } else {
+            transformedValues = transform(interpolationNodes, values);
+        }
+
+        Eigen::VectorXd solution;
+        if (splineConstraints_->fitData_) {
+            const Integer firstRow = static_cast<Integer>(nConstraintsBefore);
+            const Integer lastRow =
+                static_cast<Integer>(nConstraintsBefore + transformedValues.size());
+            interpolationA_ = splineConstraints_->getSliceOfA(firstRow, lastRow);
+
+            splineConstraints_->pop();
+
+            interpolationBVec_ = Eigen::Map<const Eigen::VectorXd>(transformedValues.data(),
+                                                                   transformedValues.size());
+
+            if (firstRow == 0) {
+                Eigen::SparseQR<Eigen::SparseMatrix<Real>, Eigen::COLAMDOrdering<int>> solver;
+                solver.compute(interpolationA_);
+                solution = solver.solve(interpolationBVec_);
+                QL_REQUIRE(solver.info() == Eigen::Success, "Solver failed");
+            } else {
+                Eigen::SparseMatrix<Real> P = interpolationA_.transpose() * interpolationA_;
+                Eigen::VectorXd c = -(interpolationA_.transpose() * interpolationBVec_);
+                // Problem is 1/2 x^t P x + c x, with P = A^tA, c = -A^t b for
+                // 1/2||Ax - b||^2
+                splineConstraints_->setP(P);
+                splineConstraints_->setCVector(c);
+
+                solution = solve(std::vector<Real>(0));
+            }
+        } else {
+            solution = solve(transformedValues);
+
+            splineConstraints_->pop();
+        }
         return solution;
+    }
+
+    // ReSharper disable once CppInconsistentNaming
+    std::vector<Real> BSplineStructure::interpolate_swig(const std::vector<Real>& interpolationNodes,
+                                                         const std::vector<Real>& values) {
+        Eigen::VectorXd result = interpolate(interpolationNodes, values);
+        return {result.data(), result.data() + result.size()};
+    }
+
+    std::vector<std::vector<Real>> BSplineStructure::get_interpolation_a() const {
+        // Initialize the output vector
+        std::vector<std::vector<Real>> result(this->interpolationA_.rows(),
+                                              std::vector<Real>(this->interpolationA_.cols(), 0.0));
+
+        // Convert the sparse matrix to a dense std::vector<std::vector<Real>>
+        for (int k = 0; k < this->interpolationA_.outerSize(); ++k) {
+            for (Eigen::SparseMatrix<Real>::InnerIterator it(this->interpolationA_, k); it; ++it) {
+                result[it.row()][it.col()] = it.value();
+            }
+        }
+
+        return result;
+    }
+
+    std::vector<Real> BSplineStructure::get_interpolation_b() const {
+        return {interpolationBVec_.data(), interpolationBVec_.data() + interpolationBVec_.size()};
     }
 
     Eigen::VectorXd BSplineStructure::solve(const std::vector<Real>& parameters) const {
@@ -88,17 +219,39 @@ namespace QuantLib {
         return splineConstraints_->getSolution();
     }
 
-    Eigen::VectorXd BSplineStructure::evaluateAll(Real x) const {
-        Size nSegments = splineSegments_.size();
-        Size nVariables = splineConstraints_->getNumVariables();
+    // ReSharper disable once CppInconsistentNaming
+    std::vector<Real>
+    BSplineStructure::solve_swig(const std::vector<Real>& parameters) const {
+        Eigen::VectorXd result = solve(parameters);
+        return {result.data(), result.data() + result.size()};
+    }
+
+
+    Eigen::VectorXd BSplineStructure::evaluateAll(const Real x,
+                                                  const BSplineSegment::Side side) const {
+        QL_REQUIRE(side == BSplineSegment::Side::Right || side == BSplineSegment::Side::Left,
+                   "Sidedness needs to be 'Right' or 'Left");
+        const Size nSegments = splineSegments_.size();
+        const Size nVariables = splineConstraints_->getNumVariables();
         Size j = 0;
         Eigen::VectorXd result = Eigen::VectorXd::Zero(nVariables);
 
         // TODO endpoints are not handled correctly, use side variables
         for (Size i = 0; i < nSegments; ++i) {
-            if (splineSegments_[i]->range().first <= x && x < splineSegments_[i]->range().second) {
-                Eigen::VectorXd segmentResult = splineSegments_[i]->evaluateAll(x);
-                result.segment(j, splineSegments_[i]->getNumVariables()) = segmentResult;
+            if (side == BSplineSegment::Side::Right) {
+                if (splineSegments_[i]->range().first <= x &&
+                    x < splineSegments_[i]->range().second) {
+                    const Eigen::VectorXd segmentResult =
+                        splineSegments_[i]->evaluateAll(x, -1, side);
+                    result.segment(j, splineSegments_[i]->getNumVariables()) = segmentResult;
+                }
+            } else {
+                if (splineSegments_[i]->range().first < x &&
+                    x <= splineSegments_[i]->range().second) {
+                    const Eigen::VectorXd segmentResult =
+                        splineSegments_[i]->evaluateAll(x, -1, side);
+                    result.segment(j, splineSegments_[i]->getNumVariables()) = segmentResult;
+                }
             }
             j += splineSegments_[i]->getNumVariables();
         }
@@ -106,38 +259,71 @@ namespace QuantLib {
         return result;
     }
 
+    // ReSharper disable once CppInconsistentNaming
+    std::vector<Real> BSplineStructure::evaluate_all(Real x, BSplineSegment::Side side) const {
+        Eigen::VectorXd result = evaluateAll(x, side);
+        return {result.data(), result.data() + result.size()};
+    }
+
+
     Real BSplineStructure::value(const Eigen::VectorXd& coefficients,
-                                            Real x,
-                                            BSplineSegment::Side side) const {
-        Size numSegments = splineSegments_.size();
+                                 Real x,
+                                 Integer nu,
+                                 BSplineSegment::Side side) const {
+        const Size numSegments = this->splineSegments_.size();
+        const Size e = nu >= 0 ? 0 : static_cast<Size>(-nu);
         Size mu = 0;
         Size cumVariables = 0;
+
+        Eigen::VectorXd extraCoefficients = Eigen::VectorXd::Zero(e);
+        Eigen::VectorXd augmentedCoefficients;
         for (Size i = 0; i < numSegments; ++i) {
-            if (splineSegments_[i]->range().first <= x && x < splineSegments_[i]->range().second) {
+            const auto splineSegment = this->splineSegments_[i];
+            if (splineSegment->range().first <= x && x < splineSegment->range().second) {
                 mu = i;
                 break;
             }
-            cumVariables += splineSegments_[i]->getNumVariables();
+            const Size nVars = splineSegment->getNumVariables();
+            augmentedCoefficients = Eigen::VectorXd::Zero(nVars + e);
+            augmentedCoefficients.segment(0, nVars) = coefficients.segment(cumVariables, nVars);
+            augmentedCoefficients.segment(nVars, e) = extraCoefficients;
+            for (Size j = 0; j < e; ++j) {
+                extraCoefficients[j] = splineSegment->value(
+                    augmentedCoefficients, splineSegment->range().second, nu + static_cast<Integer>(j), BSplineSegment::Side::Left);
+            }
+            cumVariables += nVars;
         }
-        return splineSegments_[mu]->value(
-            coefficients.segment(cumVariables, splineSegments_[mu]->getNumVariables()), x, side);
+        const auto splineSegment = this->splineSegments_[mu];
+        const Size nVars = splineSegment->getNumVariables();
+
+        augmentedCoefficients = Eigen::VectorXd::Zero(nVars + e);
+        augmentedCoefficients.segment(0, nVars) = coefficients.segment(cumVariables, nVars);
+        augmentedCoefficients.segment(nVars, e) = extraCoefficients;
+
+        return splineSegment->value(augmentedCoefficients, x, nu, side);
+    }
+
+    Real BSplineStructure::value(const std::vector<Real>& coefficients,
+                                 Real x,
+                                 Integer nu,
+                                 BSplineSegment::Side side) const {
+        return value(Eigen::Map<const Eigen::VectorXd>(coefficients.data(), coefficients.size()), x,
+                     nu, side);
     }
 
     std::pair<Real, Real> BSplineStructure::range() const {
-        Real left = splineSegments_.front()->range().first;
-        Real right = splineSegments_.back()->range().second;
+        Real left  = this->splineSegments_.front()->range().first;
+        Real right = this->splineSegments_.back()->range().second;
 
         return {left, right};
     }
 
-    void BSplineStructure::setConstraints(const ext::shared_ptr<SplineConstraints>& splineConstraints) {
-        splineConstraints_ = splineConstraints;
-    }
-
     std::vector<Real> BSplineStructure::transform(const std::vector<Real>& abscissae,
-                                                  const std::vector<Real>& values) const {
+                                                  const std::vector<Real>& values,
+                                                  BSplineSegment::Side side) const {
 
         // Check if abscissae is sorted and vectors are of same length
+        // TODO remove for performance reasons, do this check only where needed
         QL_REQUIRE(std::is_sorted(abscissae.begin(), abscissae.end()),
                    "Abscissae must be sorted in ascending order");
         QL_REQUIRE(abscissae.size() == values.size(),
@@ -160,7 +346,8 @@ namespace QuantLib {
             while (segmentIndex < numSegments) {
                 auto [left, right] = splineSegments_[segmentIndex]->range();
                 // TODO endpoints are not handled correctly, use side variables
-                if ((x < left && segmentIndex == 0) || (x >= left && x < right) ||
+                if ((x < left && segmentIndex == 0) || ((x >= left && x < right) && side == BSplineSegment::Side::Right) ||
+                    ((x > left && x <= right) && side == BSplineSegment::Side::Left) ||
                     (segmentIndex == numSegments - 1 && x >= right)) {
                     // Apply the transformation
                     transformedValues[i] = splineSegments_[segmentIndex]->transform(x, value);
@@ -181,5 +368,12 @@ namespace QuantLib {
     Eigen::VectorXd BSplineStructure::getSolution() const {
         return splineConstraints_->getSolution();
     }
+
+    // ReSharper disable once CppInconsistentNaming
+    std::vector<Real> BSplineStructure::get_solution() const {
+        Eigen::VectorXd result = splineConstraints_->getSolution();
+        return {result.data(), result.data() + result.size()};
+    }
+
 
 }
--- ql/math/interpolations/bsplineinterpolation/bsplinestructure.hpp
+++ ql/math/interpolations/bsplineinterpolation/bsplinestructure.hpp
@@ -28,7 +28,6 @@
 #include "bsplineevaluator.hpp"
 #include "splineconstraints.hpp"
 #include "splinesegment.hpp"
-#include "splinesegment.hpp"
 #include <ql/shared_ptr.hpp>
 #include <ql/types.hpp>
 #include <vector>
@@ -45,10 +44,14 @@ namespace QuantLib {
          * constraints.
          * @param splineSegments Vector of shared pointers to B-spline structures.
          * @param splineConstraints Shared pointer to spline constraints.
+         * @param useSegmentNodes Whether two-sided conditions should be imposed at segment nodes
+         * @param rejectZeroNode Whether to reject automatic addition of node at time 0.0
          */
         BSplineStructure(
             const std::vector<ext::shared_ptr<BSplineSegment>>& splineSegments,
-            const ext::shared_ptr<SplineConstraints>& splineConstraints);
+            const ext::shared_ptr<SplineConstraints>& splineConstraints,
+            bool useSegmentNodes = false,
+            bool rejectZeroNode = true);
 
         /*!
          * \brief Default constructor for BSplineStructure.
@@ -65,65 +68,102 @@ namespace QuantLib {
          * \brief Set the constraints for the spline.
          * \param splineConstraints Shared pointer to spline constraints.
          */
-        void setConstraints(const ext::shared_ptr<SplineConstraints>& splineConstraints);
+        void setConstraints(const ext::shared_ptr<SplineConstraints>& splineConstraints) {
+            this->splineConstraints_ = splineConstraints;
+        }
+
+        ext::shared_ptr<SplineConstraints> getConstraints() const { return this->splineConstraints_; }
+
 
         /*!
          * \brief Transforms values according to the transforms applicable for the spline.
          * \param abscissae The abscissa values for the points
          * \param values The values to be transformed
+         * \param side Side of value, this affects which segment's transform function is used for segment nodes.
          */
         std::vector<Real> transform(const std::vector<Real>& abscissae,
-                                    const std::vector<Real>& values) const;
+                                    const std::vector<Real>& values,
+                                    BSplineSegment::Side side = BSplineSegment::Side::Right) const;
 
         /*!
          * \brief Add interpolation nodes to the spline.
          * \param interpolationNodes Vector of interpolation nodes.
+         * \param side Sidedness of constraint
+         * \param nParameters When stacking nodes need to pass in how many parameters have been stacked
          */
-        void addInterpolationNodes(const std::vector<Real>& interpolationNodes) const;
+        void addInterpolationNodes(const std::vector<Real>& interpolationNodes,
+                                   BSplineSegment::Side side = BSplineSegment::Side::Right, Size nParameters = 0) const;
 
         /*!
          * \brief Interpolate the spline at given nodes and values.
          * \param interpolationNodes Vector of interpolation nodes.
          * \param values Vector of values at the nodes.
-         * \return A vector of interpolated values.
+         * \return A vector of spline coefficients for interpolating curve.
          */
         Eigen::VectorXd interpolate(const std::vector<Real>& interpolationNodes,
-                                    const std::vector<Real>& values) const;
+                                    const std::vector<Real>& values);
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<Real> interpolate_swig(const std::vector<Real>& interpolationNodes,
+                                           const std::vector<Real>& values);
 
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<std::vector<Real>> get_interpolation_a() const;
+
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<Real> get_interpolation_b() const;
         /*!
          * \brief Solve the spline for given parameters.
          * \param parameters Vector of parameters.
          * \return A vector of solutions.
          */
         Eigen::VectorXd solve(const std::vector<Real>& parameters) const;
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<Real> solve_swig(const std::vector<Real>& parameters) const;
 
         /*!
          * \brief Get the solution of the spline.
          * \return A vector representing the solution.
          */
         Eigen::VectorXd getSolution() const;
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<Real> get_solution() const;
+
 
         /*!
          * \brief Get the number of variables in the spline.
          * \return The number of variables.
          */
         Size getNumVariables() const { return splineConstraints_->getNumVariables(); }
+        // ReSharper disable once CppInconsistentNaming
+        Integer get_num_variables() const { return static_cast<Integer>(splineConstraints_->getNumVariables()); }
 
         /*!
          * \brief Evaluate the spline structure at a given point.
          * \param x The point at which to evaluate.
+         * \param side Sidedness of evaluation
          * \return Vector of evaluated values over spline basis functions.
          */
-        Eigen::VectorXd evaluateAll(Real x) const;
+        Eigen::VectorXd evaluateAll(Real x, BSplineSegment::Side side = BSplineSegment::Side::Right) const;
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<Real>
+        evaluate_all(Real x, BSplineSegment::Side side = BSplineSegment::Side::Right) const;
 
         /*!
          * \brief Evaluate the spline structure at a given point.
          * \param coefficients Coefficients of the spline
          * \param x The point at which to evaluate.
+         * \param nu Number of derivatives, defaults to 0 corresponding to no derivative.
          * \param side The side for evaluation.
          * \return Vector of evaluated values.
          */
-        Real value(const Eigen::VectorXd& coefficients, Real x, BSplineSegment::Side side=BSplineSegment::Side::Right) const;
+        Real value(const Eigen::VectorXd& coefficients,
+                   Real x,
+                   Integer nu = 0,
+                   BSplineSegment::Side side = BSplineSegment::Side::Right) const;
+        Real value(const std::vector<Real>& coefficients,
+                   Real x,
+                   Integer nu = 0,
+                   BSplineSegment::Side side = BSplineSegment::Side::Right) const;
 
         /*!
          * \brief Get the spline segments.
@@ -133,11 +173,24 @@ namespace QuantLib {
             return splineSegments_;
         }
 
+        // ReSharper disable once CppInconsistentNaming
+        std::vector<ext::shared_ptr<BSplineSegment>> get_spline_segments() const {
+            return splineSegments_;
+        }
+
       private:
         std::vector<ext::shared_ptr<BSplineSegment>> splineSegments_;
         ext::shared_ptr<SplineConstraints> splineConstraints_;
 
+        bool rejectZeroNode_ = true;
+        bool useSegmentNodes_ = false;
+        std::vector<Real> segmentNodes_;
+
         BSplineEvaluator spline_;
+        Real tolerance_ = 0.0e-14;
+
+        Eigen::SparseMatrix<Real> interpolationA_;
+        Eigen::VectorXd interpolationBVec_;
     };
 }
 #endif // composite_spline_structure_hpp
